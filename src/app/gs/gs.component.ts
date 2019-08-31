ed4e60a2061916f5127be40e1ffd57f339504cfe
Krasivaya <semwagacarine@gmail.com>
2019-08-31 19:55:57 +0200

    Add search button

--- src/app/gs/gs.component.ts
+++ src/app/gs/gs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../gs-user-class/user';
 import { environment } from 'src/environments/environment';
+import { Repo } from '../gs-repo-class/repo';
 
 @Component({
   selector: 'app-gs',
@@ -10,6 +11,8 @@ import { environment } from 'src/environments/environment';
 })
 export class GsComponent implements OnInit {
 
+  user:User;
+  repo: Repo;
   addUser(newUser:string){
     interface ApiResponse1 {
       avatar_url: any;
@@ -19,16 +22,21 @@ export class GsComponent implements OnInit {
       created_at: Date;
       public_repos: number;
     }
-    this.http.get<ApiResponse1>("https://api.github.com/users/"+newUser+"?access_token="+(environment.key)).subscribe(data => {
-      this.user = new User(data.avatar_url, data.login, data.followers, data.following, data.created_at, data.public_repos)
+    this.http.get<ApiResponse1>("https://api.github.com/users/"+newUser+"?access_token="+(environment.key)).subscribe(data1 => {
+      this.user = new User(data1.avatar_url, data1.login, data1.followers, data1.following, data1.created_at, data1.public_repos)
     })
-    // interface ApiResponse2 {
-    //   name:
 
-    // }
+    interface ApiResponse2 {
+      name: string;
+      description: string;
+      language: string;
+      html_url: any;
+    }
+    this.http.get<ApiResponse2>("https://api.github.com/users/"+newUser+"/repos").subscribe(data2 => {
+      this.repo = new Repo(data2.name, data2.description, data2.language, data2.html_url)
+    })
   }
 
-  user:User;
 
   constructor(
     private http:HttpClient,) { }
