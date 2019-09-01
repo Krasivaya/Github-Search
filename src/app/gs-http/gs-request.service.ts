8770ceb1a7aef45e77439964ecb638e91503f375
Krasivaya <semwagacarine@gmail.com>
2019-09-01 18:55:20 +0200

    Add about component

--- src/app/gs-http/gs-request.service.ts
+++ src/app/gs-http/gs-request.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment";
+// import { environment } from "src/environments/environment";
 import { User } from "../gs-user-class/user";
 import { Repo } from '../gs-repo-class/repo';
 
@@ -24,7 +24,7 @@ export class GsRequestService {
       // html_url:any;
     }
     // return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"?access_token="+(environment.key))
-    return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"?access_token=33b89d0a6f877fb4d26919aa947f3d3075a5e1bf")
+    return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"?access_token=e80cb3652823dadf253c3f12792ab124a1ea141d")
   }
   updateUser(newUser:string) {
     this.newUser = newUser;
@@ -37,6 +37,6 @@ export class GsRequestService {
       // html_url: any;
     }
     // return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"/repos?access_token="+(environment.key))
-    return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"/repos?access_token=33b89d0a6f877fb4d26919aa947f3d3075a5e1bf")
+    return this.http.get<ApiResponse[]>("https://api.github.com/users/"+this.newUser+"/repos?access_token=e80cb3652823dadf253c3f12792ab124a1ea141d")
   }
 }
